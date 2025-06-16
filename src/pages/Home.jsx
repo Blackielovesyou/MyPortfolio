ac2bc22c720ccb3c8309cd94d703fe1bb86d9a95
Blackielovesyou <114041791+Blackielovesyou@users.noreply.github.com>
2025-06-17 01:08:27 +0800

    push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ import { HeroSection } from "../components/HeroSection";
 import { AboutSection } from "@/components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
 import { ProjectsSection } from "../components/ProjectsSection";
+import { ContactSection } from "../components/Contact.Section";
+import { Footer } from "../components/Footer";
 
 
 export const Home = () => {
@@ -24,11 +26,13 @@ export const Home = () => {
          <AboutSection/>
          <SkillsSection/>
          <ProjectsSection/>
+         <ContactSection />
       </main>
 
       {/* Main Content */}
 
       {/* Footer */}
+     <Footer/>
     </div>
   );
 };
